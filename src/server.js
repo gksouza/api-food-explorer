bf45b93015b2001818c27573240e6c6f096a59e2
gksouza <gkoliver2020@gmail.com>
2024-03-20 19:27:38 -0300

    configurated server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require("express")
+const migrationsRun = require("./database/sqlite/migrations");
+const routes = require("./routes");
+
+migrationsRun();
 
 const app = express()
+app.use(express.json())
+app.use(routes)
 
 const PORT = process.env.PORT || 3333
+
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
